219e328a833cae04f27565c86e73e77d1ab32984
fahad3434 <fahadmalik3063@gmail.com>
2025-04-29 17:10:27 +0500

    update userdto

--- MangaBackend-Application/DTOS/Tb_UserDtos/UserDto.cs
+++ MangaBackend-Application/DTOS/Tb_UserDtos/UserDto.cs
@@ -9,7 +9,8 @@ namespace MangaBackend_Application.DTOS.Tb_UserDtos
 {
     public class UserDto
     {
-       
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
         public string? Id { get; set; }
         public string? Username { get; set; }
         public string? Email { get; set; }
